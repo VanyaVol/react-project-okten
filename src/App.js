6647f7c2fff757c6a9d74faf5087a5d8ede8385f
VanyaVol <vanya.streetworkout.sw.vol@gmail.com>
2022-01-27 20:43:28 +0200

    finish bonus_3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import './App.css';
+import {useReducer, useState} from "react";
+
+// const reducer = (state, action) => {
+//
+//     return state;
+// }
+
+
 
 function App() {
+    // const [state, dispatch] = useReducer(reducer, {cat: '', dog: ''});
 
-    return (
-        <div className="App">
+    const [form, setForm] = useState({cat: '', dog: 0});
 
-            React Project
-            
-        </div>
-    );
+    const submit = (e) => {
+        e.preventDefault();
+
+
+    }
+
+    const addCat = () => {
+    }
+
+    const addDog = () => {
+
+    }
+
+    return (<div className="App">
+
+        <form onSubmit={submit}>
+            <input name={'cat'} type="text"/>
+            <button onClick={()=>{addCat()}}>Add cat...</button>
+            <input name={'dog'} type="text"/>
+            <button onClick={()=>{addDog()}}>Add dog...</button>
+        </form>
+
+    </div>);
 }
 
 export default App;
